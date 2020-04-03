6f50f770e97b73f706f491cc326004ec54271ad3
Ariel Farias <ariel@ephealth.com.br>
2020-04-03 18:06:35 -0300

    Criado método de envio de mensagem

--- app/src/main/java/br/com/ephealth/chatapp/MessageActivity.java
+++ app/src/main/java/br/com/ephealth/chatapp/MessageActivity.java
@@ -2,13 +2,18 @@ package br.com.ephealth.chatapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -17,11 +22,15 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+
 import br.com.ephealth.chatapp.db.IFirebase;
 import br.com.ephealth.chatapp.db.model.User;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import de.hdodenhof.circleimageview.CircleImageView;
+import es.dmoral.toasty.Toasty;
 
 public class MessageActivity extends AppCompatActivity {
 
@@ -31,11 +40,20 @@ public class MessageActivity extends AppCompatActivity {
     TextView textViewUserName;
     @BindView(R.id.toolbar)
     MaterialToolbar toolbar;
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+    @BindView(R.id.buttonSend)
+    ImageButton buttonSend;
+    @BindView(R.id.textInputEditTextMessage)
+    TextInputEditText textInputEditTextMessage;
+    @BindView(R.id.textInputLayout)
+    TextInputLayout textInputLayout;
 
     FirebaseUser firebaseUser;
     DatabaseReference reference;
 
     Intent intent;
+    private String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +61,7 @@ public class MessageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_message);
         ButterKnife.bind(this);
 
+        textInputLayout.setHint("");
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("");
@@ -52,7 +71,7 @@ public class MessageActivity extends AppCompatActivity {
         });
 
         intent = getIntent();
-        String userId = intent.getStringExtra(IntentParameters.userID);
+        userId = intent.getStringExtra(IntentParameters.userID);
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         reference = FirebaseDatabase.getInstance().getReference(IFirebase.USERS).child(userId);
 
@@ -73,5 +92,26 @@ public class MessageActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void sendMessage(String sender, String receiver, String message) {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put(IFirebase.SENDER, sender);
+        hashMap.put(IFirebase.RECEIVER, receiver);
+        hashMap.put(IFirebase.MESSAGE, message);
+        reference.child(IFirebase.CHATS).push().setValue(hashMap);
+
+    }
+
+    @OnClick(R.id.buttonSend)
+    public void onViewClicked() {
+        String message = textInputEditTextMessage.getText().toString();
+        if (!message.isEmpty()) {
+            sendMessage(firebaseUser.getUid(), userId, message);
+        } else {
+            Toasty.warning(this, R.string.cannotSendEmptyMessage, Toast.LENGTH_SHORT).show();
+        }
+        textInputEditTextMessage.setText("");
     }
 }
--- app/src/main/java/br/com/ephealth/chatapp/db/IFirebase.java
+++ app/src/main/java/br/com/ephealth/chatapp/db/IFirebase.java
@@ -3,4 +3,8 @@ package br.com.ephealth.chatapp.db;
 public interface IFirebase {
     String USERS = "Users";
     String DEFAULT = "default";
+    String CHATS = "Chats";
+    String SENDER = "sender";
+    String RECEIVER = "receiver";
+    String MESSAGE = "message";
 }
